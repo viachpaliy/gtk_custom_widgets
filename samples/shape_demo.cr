a65855b3e93e7fa60ed2e61d86f3376b88e69853
viachpaliy <viachpaliy@ukr.net>
2020-05-03 16:24:36 +0300

    fix error in Color class

--- samples/shape_demo.cr
+++ samples/shape_demo.cr
@@ -2,18 +2,26 @@ require "gobject/gtk/autorun"
 require "cairo-gobject/cairo"
 require "gtk_custom_widgets"
 
+alias GCW = GtkCustomWidgets
+
 window = Gtk::Window.new(title: "DataBar demo", border_width: 10)
 window.connect "destroy", &->Gtk.main_quit
 window.resize 500,300
-s_p = GtkCustomWidgets::Point.new 10, 20
-e_p = GtkCustomWidgets::Point.new 480, 280
-lg = GtkCustomWidgets::LineGeometry.new s_p, e_p
-br = GtkCustomWidgets::LineStyle.new
-br.color = GtkCustomWidgets::Color.new 0.0, 0.0, 1.0
+s_p = GCW::Point.new 10, 20
+e_p = GCW::Point.new 480, 280
+lg = GCW::LineGeometry.new s_p, e_p
+br = GCW::LineStyle.new
+br.color = GCW::Color.new "darkcyan"
+br.width = 10
 lg.brush = br
-line = GtkCustomWidgets::Shape(GtkCustomWidgets::LineGeometry).new
+line = GCW::Shape(GCW::LineGeometry).new
 line.geometry = lg
-window.add line
+l2 = GCW::LineGeometry.new GCW::Point.new(10, 80), GCW::Point.new(450, 80)
+l2.brush = br
+line2 = GCW::Shape(GCW::LineGeometry).new
+line2.geometry = l2
+line2.add line
+window.add line2
 
 window.show_all
 
--- src/core/color.cr
+++ src/core/color.cr
@@ -20,12 +20,83 @@ module GtkCustomWidgets
       initialize(color.red / 65535, color.green / 65535, color.blue / 65535)
     end
 
-    def self.new_from_bytes(red : UInt8 = 0, green : UInt8 = 0, blue : UInt8 = 0, alfa : UInt8 = 255) : self 
-      new(Float64.new(red / 255), Float64.new(green / 255), Float64.new(blue / 255), Float64.new(alfa / 255))
+    def initialize(style : String)
+      r = 0.0
+      g = 0.0
+      b =0.0
+      a = 1.0
+      # rgb(255,0,0)
+      if /^rgb\((\d+), ?(\d+), ?(\d+)\)$/i =~ style 
+        r = [255.0, $1.to_f].min / 255.0
+        g = [255.0, $2.to_f].min / 255.0
+        b = [255.0, $3.to_f].min / 255.0
+        a = 1.0
+      end
+      # rgba(255,0,0,1)
+      if /^rgba\((\d+), ?(\d+), ?(\d+), ?(\d+)\)$/i =~ style
+        r = [255.0, $1.to_f].min / 255.0
+        g = [255.0, $2.to_f].min / 255.0
+        b = [255.0, $3.to_f].min / 255.0
+        a = [255.0, $4.to_f].min / 255.0
+      end
+      # rgb(100%,0%,0%)
+      if /^rgb\((\d+)\%, ?(\d+)\%, ?(\d+)\%\)$/i =~ style
+        r = [100.0, $1.to_f].min / 100.0
+        g = [100.0, $2.to_f].min / 100.0
+        b = [100.0, $3.to_f].min / 100.0
+        a = 1.0
+      end
+      # rgba(100%,0%,0%,100%)
+      if /^rgba\((\d+)\%, ?(\d+)\%, ?(\d+)\%, ?(\d+)\%\)$/i =~ style
+        r = [100.0, $1.to_f].min / 100.0
+        g = [100.0, $2.to_f].min / 100.0
+        b = [100.0, $3.to_f].min / 100.0
+        a = [100.0, $4.to_f].min / 100.0
+      end
+      # #ff0000
+      if /^\#([0-9a-f]{6})$/i =~ style
+        hb = $1.hexbytes
+        r = hb[0] / 255.0
+        g = hb[1] / 255.0
+        b = hb[2] / 255.0
+        a = 1.0
+      end
+      # #ff0000ff
+      if /^\#([0-9a-f]{8})$/i =~ style
+        hb = $1.hexbytes
+        r = hb[0] / 255.0
+        g = hb[1] / 255.0
+        b = hb[2] / 255.0
+        a = hb[3] / 255.0
+      end
+      # #f00
+      if /^\#([0-9a-f])([0-9a-f])([0-9a-f])$/i =~ style
+        hb = ($1 + $1 + $2 + $2 + $3 + $3).hexbytes
+        r = hb[0] / 255.0
+        g = hb[1] / 255.0
+        b = hb[2] / 255.0
+      end
+      # #f00f
+      if /^\#([0-9a-f])([0-9a-f])([0-9a-f])([0-9a-f])$/i =~ style
+        hb = ($1 + $1 + $2 + $2 + $3 + $3 + $4 +$4).hexbytes
+        r = hb[0] / 255.0
+        g = hb[1] / 255.0
+        b = hb[2] / 255.0
+        a = hb[3] / 255.0
+       end
+      # red
+      if /^(\w+)$/i =~ style
+        hex = ColorKeywords[style].floor
+        r = (hex >> 16 & 255) / 255.0
+        g = (hex >> 8 & 255) / 255.0
+        b = (hex & 255) / 255.0
+        a = 1.0
+      end
+      initialize(r, g, b, a)
     end
 
-    def self.new(style : String) : self
-      new.set_style(style).as Color 
+    def self.new_from_bytes(red : UInt8 = 0, green : UInt8 = 0, blue : UInt8 = 0, alfa : UInt8 = 255) : self 
+      new(Float64.new(red / 255), Float64.new(green / 255), Float64.new(blue / 255), Float64.new(alfa / 255))
     end
 
     def set_hex_rgb(hex)
@@ -73,7 +144,8 @@ module GtkCustomWidgets
 
     def set_style(style)
       # rgb(255,0,0)
-      if /^rgb\((\d+), ?(\d+), ?(\d+)\)$/i =~ style
+      if /^rgb\((\d+), ?(\d+), ?(\d+)\)$/i =~ style 
+        puts "rgb"
         red = [255.0, $1.to_f].min / 255.0
         green = [255.0, $2.to_f].min / 255.0
         blue = [255.0, $3.to_f].min / 255.0
