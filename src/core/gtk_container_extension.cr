6a72f9d02374c3833021002eda56debac1bdb0d1
viachpaliy <viachpaliy@ukr.net>
2020-07-15 23:12:51 +0300

    rework extension for Container for gobject v0.8.0

--- src/core/gtk_container_extension.cr
+++ src/core/gtk_container_extension.cr
@@ -7,8 +7,8 @@ module Gtk
       list = LibGtk.container_get_children(@pointer.as(LibGtk::Container*)).as(LibGLib::List*)
       if !list.value.data.null?
         childs << Gtk::Widget.new list.value.data.as(LibGtk::Widget*)
-        while !list.value.next_.null?
-          list = list.value.next_.as(LibGLib::List*)
+        while !list.value._next.null?
+          list = list.value._next.as(LibGLib::List*)
           if !list.value.data.null?
             childs << Gtk::Widget.new list.value.data.as(LibGtk::Widget*)
           end
